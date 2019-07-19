62106a12d1d6a6b4c9d81625fb5e1ef5c7b13bf5
Shaila Zaman <shaila.zaman96@gmail.com>
2019-07-19 10:40:13 -0500

    finished noise reducing in default manner

--- scripts/us-common-functions.R
+++ scripts/us-common-functions.R
@@ -13,15 +13,27 @@ performance_data_dir <- 'performance-data'
 index_data_dir <- 'index-data'
 final_data_dir <- 'final-data'
 
-subj_list_file_name <- 'subj_list.csv'
-
-
 default_plot_width = 12
 default_plot_height = 10
 
 
+subj_list_file_name <- 'subj_list.csv'
+
+
 grp_dir <- 'Group1'
 session_list <- c('Baseline', 'WorkingSession')
+
+
+pp_file_pattern <- '.*_pp.csv'
+nr_pp_file_pattern <- '.*_nr.csv'
+marker_file_pattern <- '.*_sessionmarkers.csv'
+e4_file_pattern <- 'HR.csv|EDA.csv'
+
+s_interface_date_format <- '%a %b %d %H:%M:%S'
+
+
+
+
 
 
 
@@ -48,6 +60,11 @@ print_msg <- function(msg) {
   message(msg)
 }
 
+write_log_msg <- function(msg, file_name) {
+  message(msg)
+  write(msg, file=file_name, append=TRUE)
+}
+
 is_match <- function(str, pattern) { 
   return(grepl(pattern, str)) 
 } 
@@ -68,11 +85,15 @@ replace_to_space <- function(str) {
 #--------------------------#
 #--- File and Directory ---#
 #--------------------------#
-is_empty_dir <- function(list_dir) {
-  return(length(list_dir)==0)
+is_empty <- function(item) {
+  return(length(item)==0)
 }
 
 get_dir_list <- function(directory) {
   return(list.dirs(path=directory, full.names=F, recursive=F))
+}
+
+get_matched_file_names <- function(directory, file_pattern) {
+  return(list.files(path=directory, pattern=file_pattern, recursive=F))
 }
 
